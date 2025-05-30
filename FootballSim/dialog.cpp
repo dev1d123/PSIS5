da19cd40ab1b92a06565fa888a2e75fea5fe432e
dev1d123 <dhuamanio@unsa.edu.pe>
2025-05-30 16:13:42 -0500

    added: behavior

--- FootballSim/dialog.cpp
+++ FootballSim/dialog.cpp
@@ -1,3 +1,17 @@
 #include "dialog.h"
+#include "ui_dialog.h"
 
-dialog::dialog() {}
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog) {
+    ui->setupUi(this);
+
+    connect(ui->pushButton, &QPushButton::clicked, this, [this]() {
+        emit mensajeEnviado("¡Hola desde la segunda ventana!");
+        this->close();
+    });
+}
+
+Dialog::~Dialog() {
+    delete ui;
+}
--- FootballSim/dialog.h
+++ FootballSim/dialog.h
@@ -1,10 +1,24 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
-class dialog
-{
+#include <QDialog>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog {
+    Q_OBJECT
+
 public:
-    dialog();
+    explicit Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+signals:
+    void mensajeEnviado(const QString &msg); // señal personalizada
+
+private:
+    Ui::Dialog *ui;
 };
 
 #endif // DIALOG_H
