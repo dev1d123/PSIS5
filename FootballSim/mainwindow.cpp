d760cb8059e7aceed36e66b9c1d2618cb5baf73f
dev1d123 <dhuamanio@unsa.edu.pe>
2025-06-01 17:17:00 -0500

    added: labels to images

--- FootballSim/mainwindow.cpp
+++ FootballSim/mainwindow.cpp
@@ -2,11 +2,16 @@
 #include "ui_mainwindow.h"
 #include "dialog.h"
 #include <QMessageBox>
-
+#include <QLabel>
+#include <QList>
+#include <QDebug>
+#include <iostream>
+#include <string>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
 
     connect(ui->pushButton, &QPushButton::clicked, this, [this]() {
         Dialog *d = new Dialog(this);
